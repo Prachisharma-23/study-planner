d48d8bfa517c414769204a6028527093b95ea4ad
Prachisharma-23 <prachisharmaa2003@gmail.com>
2025-10-28 12:49:37 +0530

    new commit

--- src/TaskListPage.js
+++ src/TaskListPage.js
@@ -33,7 +33,7 @@ export default function TaskListPage() {
       return;
     }
     axios
-      .get(`http://localhost:8080/api/tasks/${username}`)
+      .get(`https://demo-ut0u.onrender.com/api/tasks/${username}`)
       .then((response) => setTasks(response.data))
       .catch((error) => console.error("Error fetching tasks:", error));
   };
